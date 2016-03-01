65f10b24ae6425657b12e34ed7208f7628550a83
William Khim <Will@AzianMac.local>
2016-03-01 10:19:58 +0100

    Carnet d'adresses fonctionnel

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,9 +14,77 @@ class DefaultController extends Controller {
      */
     public function indexAction(Request $request) {
         if ($this->isGranted("IS_AUTHENTICATED_REMEMBERED"))
-            return $this->redirect($this->generateUrl("fos_user_profile_show"));
+            return $this->redirect($this->generateUrl("contact"));
         else
             return $this->redirect($this->generateUrl("fos_user_security_login"));
+    }
+
+    /**
+     * @Route("/contact/all", name="contact_all")
+     * @Template()
+     */
+    public function allAction() {
+        $em = $this->getDoctrine()->getManager();
+        return (array("users" => $em->getRepository("AppBundle:User")->findAll()));
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     * @Template()
+     */
+    public function myContactsAction() {
+        $security = $this->container->get('security.context');
+        $user = $security->getToken()->getUser();
+
+        return array("user" => $user);
+    }
+
+    /**
+     * @Route("/contact/show/{id}", name="contact_show")
+     * @Template()
+     */
+    public function showAction($id) {
+        $em = $this->getDoctrine()->getManager();
+        $contact = $em->getRepository("AppBundle:User")->find($id);
+
+        return array("user" => $contact);
+    }
+
+    /**
+     * @Route("/contact/add/{id}", name="contact_add")
+     */
+    public function addAction($id) {
+        $security = $this->container->get('security.context');
+        $user = $security->getToken()->getUser();
+        $em = $this->getDoctrine()->getManager();
+        $contact = $em->getRepository("AppBundle:User")->find($id);
+
+        $user->addContacts($contact);
+        $contact->addContactsWithMe($user);
+        $em->persist($user);
+        $em->persist($contact);
+        $em->flush();
+
+        return $this->redirect($this->generateUrl("contact"));
+    }
+
+    /**
+     * @Route("/contact/remove/{id}", name="contact_remove")
+     */
+    public function removeAction($id) {
+        $security = $this->container->get('security.context');
+        $user = $security->getToken()->getUser();
+        $em = $this->getDoctrine()->getManager();
+        $contact = $em->getRepository("AppBundle:User")->find($id);
+
+        $user->removeContacts($contact);
+        $contact->removeContactsWithMe($user);
+
+        $em->persist($user);
+        $em->persist($contact);
+        $em->flush();
+
+        return $this->redirect($this->generateUrl("contact"));
     }
 
 }
